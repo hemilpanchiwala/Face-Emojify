95fa00a8f095627b91e4b322698baa28702f890d
hemilpanchiwala <hpanchiwala@cs.iitr.ac.in>
2019-06-27 00:26:18 +0530

    Added the feature of identifying which imoji matches the expression of person in image

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -12,11 +12,14 @@ import com.google.android.gms.vision.face.FaceDetector;
 
 public class Emojifier {
 
+    private static final double SMILING_PROB_THRESHOLD = .5;
+    private static final double EYE_OPEN_PROB_THRESHOLD = .5;
+
     public static void detectFaces(Context context, Bitmap bitmap){
 
         FaceDetector detector = new FaceDetector.Builder(context)
                 .setTrackingEnabled(false)
-                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .build();
 
         Frame frame = new Frame.Builder().setBitmap(bitmap).build();
@@ -24,11 +27,74 @@ public class Emojifier {
         SparseArray<Face> faces = detector.detect(frame);
 
         if (faces.size() > 0) {
-            Toast.makeText(context, Integer.toString(faces.size()), Toast.LENGTH_LONG).show();
+            Toast.makeText(context, Integer.toString(faces.size()), Toast.LENGTH_SHORT).show();
         }else {
-            Toast.makeText(context, "No Face Detected", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "No Face Detected", Toast.LENGTH_SHORT).show();
         }
 
+        for (int i=0;i<faces.size();i++){
+            Face face = faces.valueAt(i);
+
+            whichEmoji(context, face);
+
+        }
+
+        detector.release();
+
+    }
+
+    public static void whichEmoji(Context context, Face face){
+        float leftEyeOpenProbability = face.getIsLeftEyeOpenProbability();
+        float rightEyeOpenProbability = face.getIsRightEyeOpenProbability();
+        float smilingProbability = face.getIsSmilingProbability();
+
+        Boolean smiling = (smilingProbability>SMILING_PROB_THRESHOLD);
+        Boolean rightEyeOpen = (rightEyeOpenProbability>EYE_OPEN_PROB_THRESHOLD);
+        Boolean leftEyeOpen = (leftEyeOpenProbability>EYE_OPEN_PROB_THRESHOLD);
+
+        Emoji emoji;
+
+        if (smiling){
+
+            if(rightEyeOpen && leftEyeOpen){
+                emoji = Emoji.SMILE;
+            }else if(!rightEyeOpen && leftEyeOpen){
+                emoji = Emoji.RIGHTWINK;
+            }else if(rightEyeOpen && !leftEyeOpen){
+                emoji = Emoji.LEFTWINK;
+            }else {
+                emoji = Emoji.CLOSED_SMILE;
+            }
+
+        }else {
+
+            if(rightEyeOpen && leftEyeOpen){
+                emoji = Emoji.FROWN;
+            }else if(!rightEyeOpen && leftEyeOpen){
+                emoji = Emoji.RIGHTWINKFROWN;
+            }else if(rightEyeOpen && !leftEyeOpen){
+                emoji = Emoji.LEFTWINKFROWN;
+            }else {
+                emoji = Emoji.CLOSED_FROWN;
+            }
+
+        }
+
+        Toast.makeText(context, emoji.name(), Toast.LENGTH_LONG).show();
+
+    }
+
+
+
+    private enum Emoji{
+        SMILE,
+        FROWN,
+        LEFTWINK,
+        RIGHTWINK,
+        LEFTWINKFROWN,
+        RIGHTWINKFROWN,
+        CLOSED_FROWN,
+        CLOSED_SMILE
     }
 
 }
